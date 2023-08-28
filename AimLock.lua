6f5ea2ffbba3c081f1314345e45ac440258bb3e5
LeakedBuffalo7907 <69818555+LeakedBuffalo7907@users.noreply.github.com>
2023-08-27 20:28:43 -0400

    Update AimLock.lua

--- AimLock.lua
+++ AimLock.lua
@@ -9,10 +9,15 @@ local AimLock = {
   turnSpeed = 0.5
 }
 
+local function Log(tbl)
+print("[Monkey Aim Debug]: " .. tbl)
+end
+
 function AimLock:Update() 
 
   while AimLock.Enable do
     local pressed = UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2)
+    log(pressed)
     if pressed then
       local currentActor = nil
       local maxDist = AimLock.fov
@@ -21,10 +26,11 @@ function AimLock:Update()
         if model:IsA("Model") and model.Name == AimLock.Filter and (not AimLock.TeamCheck or model:GetChildren("friendly_model") ~= nil) then
           local ssHeadPoint = cam:WorldToScreenPoint(model.PrimaryPart.Position)
           local distance = (Vector2.new(model.PrimaryPart.Position.X, model.PrimaryPart.Position.Y) - UserInputService:GetMouseLocation())
-
+          log(model)
           if distance < maxDist then
             currentActor = model
             maxDist = distance
+            log("new dist " .. distance .. " max " .. maxDist)
           end
           
           
@@ -42,8 +48,11 @@ function AimLock:Update()
         local currentLookVector = camera.CFrame.LookVector
         local targetLookVector = (targetPosition - camera.CFrame.Position).Unit
         local newLookVector = Vector3.lerp(currentLookVector, targetLookVector, lerpedValue)
+        log("Camera Moving")
+        log("currentLookVector: " .. currentLookVector .. " targetLookVector: " .. targetLookVector .. " newLookVector: " .. newLookVector
         camera.CFrame = CFrame.lookAt(camera.CFrame.Position, camera.CFrame.Position + newLookVector)
-        wait()
+        log("CFrame Called")
+        --wait()
       end
     end
   end
